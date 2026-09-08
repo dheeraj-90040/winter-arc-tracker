8bcf5076ec1ddf6f605ed30b6670cac7efec8a6a
WinterArc <winterarc@local>
2026-09-08 20:06:20 +0530

    Steps live only on Steps tab; date-numbered chart + today line

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'winter-arc-cache-v9';
+const CACHE_NAME = 'winter-arc-cache-v10';
 const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 // Install: pre-cache app shell
