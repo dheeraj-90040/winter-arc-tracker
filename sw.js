c9e12228ccf88caed6dad4e82346de3c702221c8
WinterArc <winterarc@local>
2026-09-08 19:42:37 +0530

    Date-wise steps: local dates, check-in merge, daily nudge

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'winter-arc-cache-v7';
+const CACHE_NAME = 'winter-arc-cache-v8';
 const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 // Install: pre-cache app shell
