11e7ec1eab5ed56b777c3d3d1d21fb3fd51f4e65
dheeraj-90040 <dheeraj-90040@users.noreply.github.com>
2026-09-09 00:50:52 +0530

    Fix week toggle phantom post-week + nav scroll jump (v14)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'winter-arc-cache-v13';
+const CACHE_NAME = 'winter-arc-cache-v14';
 const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 // Install: pre-cache app shell
