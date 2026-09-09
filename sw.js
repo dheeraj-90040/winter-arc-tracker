2217a7ee773acd27ac84b6401b6bbaf8478d6300
WinterArc <winterarc@local>
2026-09-09 21:09:56 +0530

    V16: import backup (fixes empty APK data migration) + weight pipeline verified

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'winter-arc-cache-v15';
+const CACHE_NAME = 'winter-arc-cache-v16';
 const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 // Install: pre-cache app shell
