6d38c85359c0ac0737c60f0b0722d1d1dde26ab2
dheeraj-90040 <2100090040@kluniversity.in>
2026-09-09 22:43:15 +0530

    V18: align schedule to approved plan (W1 Lite + Upper/Lower rotation)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'winter-arc-cache-v17';
+const CACHE_NAME = 'winter-arc-cache-v18';
 const ASSETS = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 // Install: pre-cache app shell
